c50e2973868cf183fe95b5d28fa40c3143adfa17
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-09-14 08:19:44 +0530

    refactored router from net/http to julienschmidt

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
@@ -10,22 +11,23 @@ import (
 
 func (app *application) routes() http.Handler {
 
-	mux := http.NewServeMux()
+	router := httprouter.New()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet,"/static/*filepath",http.StripPrefix("/static",fileServer))
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodGet,"/",app.home)
+	router.HandlerFunc(http.MethodGet,"/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet,"/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost,"/snippet/create", app.snippetCreatePost)
 
 	// Create a middleware chain containing our 'standard' middleware
   // which will be used for every request our application receives
 
 	// Return the 'standard' middleware chain followed by the servemux
 	standard := alice.New(app.recoverPanic,app.logRequest,secureHeaders)
-	
+
 	// secureHeaders middleware wraps the mux - every route registered to the router (mux) will get this middleware
-	return standard.Then(mux)
+	return standard.Then(router)
 
 }
