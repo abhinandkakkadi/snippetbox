66e0d46cbc22d3818604ca494d2815cfb0e04da9
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-09-17 09:07:03 +0530

    form decode helper inti

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"net/http"
 	"runtime/debug"
 	"time"
+
+	"github.com/go-playground/form/v4"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
@@ -57,5 +60,29 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
 		CurrentYear: time.Now().Year(),
 	}
+
+}
+
+// create a decoder post form helper method 
+func (app *application) decodePostForm(r *http.Request, dst any) error {
+
+	err := r.ParseForm()
+	if err != nil {
+		return err
+	}
+
+	err = app.formDecoder.Decode(dst,r.PostForm)
+	if err != nil {
+
+		var invalidDecoderError *form.InvalidDecoderError
+		if errors.As(err, &invalidDecoderError) {
+			panic(err)
+		}
+
+		// for all other errors, return them as normal
+		return err
+	}
+
+	return nil
 
 }
